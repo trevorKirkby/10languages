c175fe1b5b144f5a9ab32edaf5a89034881fb540
Trevor <TrevorKirkby123@gmail.com>
2015-07-04 13:28:47 -0700

    Solution 1 for C Complete
    Windows was being irritating with headers and compatibility, but that is
    all sorted now.

--- C/solution1.c
+++ C/solution1.c
@@ -4,7 +4,11 @@
 int main(int argc , char *argv[])
 {
     WSADATA wsa;
-     
+    SOCKET s;
+    struct sockaddr_in server;
+    char *message , server_reply[2000];
+    int recv_size;
+ 
     printf("\nInitialising Winsock...");
     if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
     {
@@ -12,7 +16,74 @@ int main(int argc , char *argv[])
         return 1;
     }
      
-    printf("Initialised.");
+    printf("Initialised.\n");
+     
+    //Create a socket
+    if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+    {
+        printf("Could not create socket : %d" , WSAGetLastError());
+    }
+ 
+    printf("Socket created.\n");
+     
+     
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 1234 );
+ 
+    //Connect to remote server
+    if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        puts("connect error");
+        return 1;
+    }
+     
+    puts("Connected");
+     
+    //Send some data
+    message = "\"hello\"\n";
+    if( send(s , message , strlen(message) , 0) < 0)
+    {
+        puts("Send failed");
+        return 1;
+    }
+    puts("Data Send\n");
+     
+    //Receive a reply from the server
+    if((recv_size = recv(s , server_reply , 2000 , 0)) == SOCKET_ERROR)
+    {
+        puts("recv failed");
+    }
+     
+    puts("Reply received\n");
+ 
+    //Add a NULL terminating character to make it a proper string before printing
+    server_reply[recv_size] = '\0';
+    puts(server_reply);
+
+    //Send some data
+    message = "\"goodbye\"\n";
+    if( send(s , message , strlen(message) , 0) < 0)
+    {
+        puts("Send failed");
+        return 1;
+    }
+    puts("Data Send\n");
+     
+    //Receive a reply from the server
+    if((recv_size = recv(s , server_reply , 2000 , 0)) == SOCKET_ERROR)
+    {
+        puts("recv failed");
+    }
+     
+    puts("Reply received\n");
+ 
+    //Add a NULL terminating character to make it a proper string before printing
+    server_reply[recv_size] = '\0';
+    puts(server_reply);
+
+    closesocket(s);
+	WSACleanup();
  
     return 0;
 }
